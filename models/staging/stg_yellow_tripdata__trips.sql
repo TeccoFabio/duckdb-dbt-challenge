a1ecf9df3bfaa3119bd6b1ec3845eac525364454
fabio <neo@kali>
2024-12-11 00:45:28 +0100

    Starting_Data_Mart_4_files

--- models/staging/stg_yellow_tripdata__trips.sql
+++ models/staging/stg_yellow_tripdata__trips.sql
@@ -1,36 +1,34 @@
---TODO: Delete in production!
-{{ config({"materialized": "table"}) }}
-
 WITH
-source AS (
-    SELECT * FROM 'data/raw/yellow_tripdata_2024-01.parquet'
-),
-
-renamed AS (
+trips AS (
     SELECT
+        --timestamps
+        tpep_pickup_datetime AS pickup_at,
+        tpep_dropoff_datetime AS dropoff_at,
+
         --ids
         VendorID AS vendor_id,
+        {{ dbt_utils.generate_surrogate_key(['vendor_id', 'pickup_at']) }} AS trip_id,
         CAST(RatecodeID AS INTEGER) AS rate_code_id,
         PULocationID AS pu_location_id,
 
         --strings
         CASE
-            WHEN trip_distance BETWEEN 0 AND 2 THEN 'short'
-            WHEN trip_distance BETWEEN 2 AND 5 THEN 'medium'
-            WHEN trip_distance > 5 THEN 'long'
+            WHEN trip_distance BETWEEN 0 AND 2 THEN 'Short'
+            WHEN trip_distance BETWEEN 2 AND 5 THEN 'Medium'
+            WHEN trip_distance > 5 THEN 'Long'
             ELSE NULL
         END AS trip_segment,
         CASE
-            WHEN date_part('hour', tpep_pickup_datetime) BETWEEN 5 AND 11 THEN 'morning'
-            WHEN date_part('hour', tpep_pickup_datetime) BETWEEN 12 AND 16 THEN 'afternoon'
-            WHEN date_part('hour', tpep_pickup_datetime) BETWEEN 17 AND 21 THEN 'evening'
-            WHEN date_part('hour', tpep_pickup_datetime) IN (22,23,0,1,2,3,4) THEN 'night'
+            WHEN date_part('hour', pickup_at) BETWEEN 5 AND 11 THEN 'Morning'
+            WHEN date_part('hour', pickup_at) BETWEEN 12 AND 16 THEN 'Afternoon'
+            WHEN date_part('hour', pickup_at) BETWEEN 17 AND 21 THEN 'Evening'
+            WHEN date_part('hour', pickup_at) IN (22,23,0,1,2,3,4) THEN 'Night'
             ELSE NULL
         END AS time_slot,
 
         --numerics
         CAST(passenger_count AS TINYINT) AS passenger_count,
-        CAST(date_diff('second', tpep_pickup_datetime, tpep_dropoff_datetime) AS FLOAT)/60 AS trip_duration,
+        ROUND(CAST(date_diff('second', pickup_at, dropoff_at) AS FLOAT)/60, 2) AS trip_duration,
         CAST(trip_distance AS FLOAT) AS trip_distance,    
         CAST(fare_amount AS FLOAT) AS fare_amount,
         CAST(tip_amount AS FLOAT) AS tip_amount,
@@ -40,15 +38,11 @@ renamed AS (
         CASE
             WHEN payment_type = 2 THEN true 
             ELSE false
-        END AS is_prepaid,
-
-        --timestamps
-        tpep_pickup_datetime::TIMESTAMP AS tpep_pickup_datetime,
-        tpep_dropoff_datetime::TIMESTAMP AS tpep_dropoff_datetime
+        END AS is_prepaid
     FROM 
-        source            
+        {{ source( "yellow_tripdata", "raw_yellow_tripdata__trips") }}            
     WHERE
         trip_duration > 0.0 AND trip_distance > 0.0 AND total_amount > 0.0 AND passenger_count <= 4
 )
 
-SELECT * FROM renamed
+SELECT * FROM trips
